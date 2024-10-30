7de9908608581c06807594a95742c82181e11d2f
Tristan Renson <tristan.renson@gmail.com>
2024-10-30 16:45:35 +0000

    nouvelle maj

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,5 @@
-SELECT *
-FROM {{ref("stg_raw__adwords")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__bing")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__criteo")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__facebook")}}
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adwords'),
+    ref('stg_raw__bing')]
+) }}
+
