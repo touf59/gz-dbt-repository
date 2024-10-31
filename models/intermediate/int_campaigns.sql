e7b5a700915401f7e8b686e4908a2187d6b8e9c4
Tristan Renson <tristan.renson@gmail.com>
2024-10-31 08:28:55 +0000

    dernier commit du jour 3110

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,5 +1,7 @@
 {{ dbt_utils.union_relations(
     relations=[ref('stg_raw__adwords'),
-    ref('stg_raw__bing')]
+    ref('stg_raw__bing'),
+    ref('stg_raw__criteo'),
+    ref('stg_raw__facebook')]
 ) }}
 
